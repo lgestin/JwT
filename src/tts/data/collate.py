b365814eb9c11308f8a7ce43c6c3ba9931a7f016
lgestin <lucas.gestin.pro@gmail.com>
2026-05-14 15:53:57 +0200

    feat(data/collate) add padding and mask computation for sequences of different lengths

--- src/tts/data/collate.py
+++ src/tts/data/collate.py
@@ -1,18 +1,44 @@
 import torch
+import torch.nn.functional as F
 
 from tts.data.dataset import Batch, Sample
+
+
+def pad_sequences_longest(
+    sequences: list[torch.Tensor], mode="constant"
+) -> tuple[list[torch.Tensor], list[int]]:
+    lengths = [seq.size(-1) for seq in sequences]
+    max_length = max(lengths)
+    padded_sequences = [
+        F.pad(seq, (0, max_length - seq.size(-1)), mode=mode) for seq in sequences
+    ]
+    return padded_sequences, lengths
+
+
+def mask_from_lengths(lengths: list[int]) -> torch.BoolTensor:
+    arange = torch.arange(max(lengths)).unsqueeze(0).repeat(len(lengths), 1)
+    mask = arange < torch.tensor(lengths).unsqueeze(1)
+    return mask
 
 
 def collate(samples: list[Sample]) -> Batch:
     idxs = [sample.idx for sample in samples]
     audios = [sample.audio for sample in samples]
-    waveforms = torch.stack([audio.waveform for audio in audios])
+    mels = [audio.mels for audio in audios]
+    padded_mels, mels_lengths = pad_sequences_longest(mels)
+    stacked_mels = torch.stack(padded_mels)
+    mels_mask = mask_from_lengths(mels_lengths)
     texts = [sample.text for sample in samples]
-    tokens = torch.stack([torch.tensor(text.tokens) for text in texts]).long()
+    tokens = [torch.tensor(text.tokens) for text in texts]
+    padded_tokens, tokens_lengths = pad_sequences_longest(tokens)
+    stacked_tokens = torch.stack(padded_tokens).long()
+    tokens_mask = mask_from_lengths(tokens_lengths)
     return Batch(
         idxs=idxs,
         audios=audios,
-        waveforms=waveforms,
+        mels=stacked_mels,
+        mels_mask=mels_mask,
         texts=texts,
-        tokens=tokens,
+        tokens=stacked_tokens,
+        tokens_mask=tokens_mask,
     )
